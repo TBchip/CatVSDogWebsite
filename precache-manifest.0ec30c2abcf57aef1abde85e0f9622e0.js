df14813989116c6bd8c7edcd96b5a761e19122fe
TBchip <Bischoffthijs@gmail.com>
2020-05-18 20:44:26 +0200

    Updates

---
+++ precache-manifest.0ec30c2abcf57aef1abde85e0f9622e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c77d1632f96bf607e8bd7862f107fff",
+    "url": "/catVSdog/index.html"
+  },
+  {
+    "revision": "b2e19497442556ed9b9e",
+    "url": "/catVSdog/static/css/main.1fb7528c.chunk.css"
+  },
+  {
+    "revision": "07727b326831a4e100b5",
+    "url": "/catVSdog/static/js/2.45fcb7f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catVSdog/static/js/2.45fcb7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e19497442556ed9b9e",
+    "url": "/catVSdog/static/js/main.68b5193e.chunk.js"
+  },
+  {
+    "revision": "457ab3372961bcc657fb",
+    "url": "/catVSdog/static/js/runtime-main.c261abfc.js"
+  }
+]);
